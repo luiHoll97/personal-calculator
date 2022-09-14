f01ab6a7e8d5a7491ae9a5d3021eed6ecbb7e433
Lui H <tech.luiholl@gmail.com>
2022-09-14 17:21:45 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { greet } from "./utils/greet";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 
 function App(): JSX.Element {
   return <h1>{greet("World")}</h1>;
